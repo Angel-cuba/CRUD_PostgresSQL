b0fc0cc2b59c69cb85bde5491ac01d2368ebef59
Mac_Developer <araozangel842@gmail.com>
2022-02-27 19:03:39 +0200

    GetById, Update and Delete

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,16 +1,44 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { connection } from '../../../utils/db';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req: NextApiRequest, res: NextApiResponse) => {
-	const { method } = req;
-
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const { method, query, body } = req;
 	switch (method) {
 		case 'GET':
-			return res.json('unique task');
+			try {
+				const querySQL = 'SELECT * FROM tasks WHERE id = $1';
+				const values = [query.id];
+				const response = await connection.query(querySQL, values);
+
+				if (response.rows.length === 0) return res.status(404).json({ message: 'Task not found' });
+				return res.json(response);
+			} catch (error: any) {
+				return res.status(400).json({ error: error.message });
+			}
 		case 'PUT':
-			return res.json('Update');
+			try {
+				const { title, description } = body;
+				const querySQL = 'UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *';
+				const values = [title, description, query.id];
+				const response = await connection.query(querySQL, values);
+
+				if (response.rowCount === 0) return res.status(404).json({ message: 'Task not found' });
+				return res.json(response);
+			} catch (error: any) {
+				return res.status(400).json({ error: error.message });
+			}
 		case 'DELETE':
-			return res.json('delete task');
+			try {
+				const querySQL = 'DELETE FROM tasks WHERE id = $1';
+				const values = [query.id];
+				const response = await connection.query(querySQL, values);
+
+				if (response.rowCount === 0) return res.status(404).json({ message: 'Task not found' });
+				return res.json(response);
+			} catch (error: any) {
+				return res.status(400).json({ error: error.message });
+			}
 		default:
 			return res.status(400).json('Method not supported');
 	}
